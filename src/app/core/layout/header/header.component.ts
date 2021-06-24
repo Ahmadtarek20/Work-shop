55de407dad34a743572da88bfd245201ae64d8b5
atarek <ahmad_tarek_abdalla2096@outlook.com>
2021-06-24 13:10:23 +0200

    seting data scss

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -8,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   hideSideBar: boolean = false;
+  openSetingMenue: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   openMenue() {
-
+    if (this.openSetingMenue == false) {
+      this.openSetingMenue = true;
+    } else {
+      this.openSetingMenue = false;
+    }
   }
   openSideMenue() {
     this.hideSideBar = true;
